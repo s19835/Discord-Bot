42abfad251292da3068f3d0578b051e5db6e5353
Maria Niranjan <130844959+s19835@users.noreply.github.com>
2024-05-26 19:27:09 +0530

    interactions are added for slash-command executions

--- bot.js
+++ bot.js
@@ -39,3 +39,23 @@ for (const folder of commandFolders) {
 }
 //some codes are differ from the documentation because we are using ES module instead of commonJS
 
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
